320f2f809849bfe6f56fc9fc851ab08559ff3f90
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-17 06:39:05 -0700

    yup

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* main comments
+ *
+ *
+ * lets add something here
+ */
+ 
+ int main(void)
+
+ {
+	 puts("Programming is like building a multilingual puzzle");
+	
+	 return (0);
+}
+	 
+
+
