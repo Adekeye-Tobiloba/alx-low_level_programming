3f92f4d4e8d0884dc050fd6711186d753096976a
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-17 10:15:03 -0700

    yup

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,9 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lt));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llt));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(it));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lit));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
+}
