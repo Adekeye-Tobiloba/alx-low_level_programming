f3c0f332e1828f6293cd0fef1ae6a42ec0c78656
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-20 15:45:25 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * mul - muliply 2 y int
+  * mul - multiply 2 y int
   * @a: first num
   * @: second num
-  * Return: result of multiplicaiton
+  * Return: result of multiplication
   */
 
 int mul(int a, int b)
