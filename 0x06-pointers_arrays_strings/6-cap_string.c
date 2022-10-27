3c479b600770044c8a4932f98d5e4c4202f602fc
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-27 15:54:35 -0700

    capitalized string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
   */
 
 char *cap_string(char *x)
-{	x = "sami, is; in learn programming africa"
+{	x = "sami, is; in learn programming africa";
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
 	int len = 13;
 	int a = 0, i;
@@ -17,9 +17,9 @@ char *cap_string(char *x)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <+ 122))
-				s[a] = s[a] - 32;
-			i++
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <+ 122))
+				x[a] = x[a] - 32;
+			i++;
 		}
 		a++;
 	}
