23cc8b2f19b1939b2bc6de49a3337bbc477a51fe
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-27 10:39:48 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _strncat -> function to append some char
+  * _strncat -> function to append some charx
   * @dest: first param
   * @src: second param
   * @n: 3rd param
@@ -9,6 +9,6 @@
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(*dest, *src, n);
+	strncat(dest, src, n);
 	return (dest);
 }
