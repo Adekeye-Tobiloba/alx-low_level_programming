0ef5a126af531734342f7dca50603eca5135c851
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-27 18:38:50 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
-#include <stdio>
 #include "main.h"
 
 /**
   * main - tests function that prints if integer is positive or negative
   * Return: 0
-  */ 
+  */
 
 int main(void)
 {
 	int i;
 
-	i = ;
-	posiitive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
 return (0);
 }
