25ba70e873ed58bcb4f99dad1e0e17e9db3bddb1
Adekeye Tobiloba <adekeyetobiloba12@gmail.com>
2022-10-28 03:35:28 -0700

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,23 +4,20 @@
   * reverse_array -> reversing an array
   * @a: array a
   * @n: an element of an array
+  * Return: no return
   */
 
 void reverse_array(int *a, int n)
 {	
-	int *p, i, aux, k;
-	a = [1, 2, 3, 4];
-	aux = 1
-	a = [4, 3, 2, 1];
-	p = a;
-	
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < n / 2; k++)
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
 	{	
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
